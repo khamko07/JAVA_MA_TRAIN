1495c3a18977eb6ffbf892deca4a97779e6fd24f
Khamko <khamko@pascaliaasia.com>
2026-03-29 15:04:46 +0700

    Initial commit

---
+++ src/main/java/com/example/java_ma_train/JavaMaTrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_ma_train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMaTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMaTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_ma_train/JavaMaTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_ma_train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMaTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
